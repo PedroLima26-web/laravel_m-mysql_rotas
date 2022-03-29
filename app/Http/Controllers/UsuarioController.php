9e85fd916c8454bf2e1e886d6179d3ea1d35b3a6
PedroLima26-web <69848820+PedroLima26-web@users.noreply.github.com>
2022-03-29 10:37:44 -0300

    Add files via upload

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function cadastrar(){
+        echo 'acessando o método cadastrar da classe UsuarioController';
+    }
+    
+}
